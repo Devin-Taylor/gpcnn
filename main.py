fc4c9f019ecb3a1b038c90d1c288014649edcc74
Devin Taylor <dev.t03@gmail.com>
2020-06-03 23:49:08 +0200

    add run scripts for gpcnn with mnist prior

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ import yaml
 from torchvision import datasets, transforms
 
 from gpcnn.dataloaders import mnist_dataloader
+from gpcnn.models.gpcnn.model import GPCNNModel
+from gpcnn.models.gpcnn.trainer import GPCNNTrainer
 from gpcnn.models.mnist.model import MNISTModel
 from gpcnn.models.mnist.trainer import MNISTTrainer
 from gpcnn.utils import mkdir
+from gpcnn.utils.model_helpers import Checkpointer
 
-MODELS = ['mnist']
+GENERATOR_MODELS = ['mnist']
+MODELS = GENERATOR_MODELS + ['gpcnn']
 RESULTS_ROOT = 'results'
 DATA_ROOT = 'data'
 MODELS_ROOT = 'models'
@@ -23,6 +27,8 @@ def parse_arguments(args_to_parse):
 
     parser.add_argument('-e', '--experiment', type=str, help='name of experiment', required=True)
     parser.add_argument('-m', '--model', type=str, help='which model to use', choices=MODELS, required=True)
+    parser.add_argument('-g', '--generator', type=str, help='which feature generator model to use', choices=GENERATOR_MODELS, required=False)
+
     parser.add_argument('--use-checkpoint', action='store_true', default=False, help='train from checkpoints')
     parser.add_argument('--cuda', action='store_true', default=False, help='enables CUDA training')
 
@@ -46,7 +52,7 @@ def main(args):
     results_path = os.path.join(RESULTS_ROOT, args.experiment)
     data_path = os.path.join(DATA_ROOT, args.model)
     settings_path = os.path.join(f'gpcnn/models/{args.model}/settings.yaml')
-    model_path = os.path.join(MODELS_ROOT, args.experiment)
+    model_path = os.path.join(MODELS_ROOT, args.model)
 
     params = get_settings(settings_path)
 
@@ -76,6 +82,59 @@ def main(args):
             use_checkpoint=args.use_checkpoint
         )
         trainer(train_loader, valid_loader, test_loader, params['train']['epochs'], device)
+    elif args.model == 'gpcnn':
+        if args.generator not in GENERATOR_MODELS:
+            raise Exception(f"Unsupported feature geneator model of type '{args.generator}'")
+
+        if args.generator == 'mnist':
+            train_loader, valid_loader = mnist_dataloader(
+                fmt='train',
+                data_path=data_path,
+                batch_size=params['train']['batch_size'],
+                validation_split=params['train']['validation_split'],
+                **kwargs
+            )
+            test_loader = mnist_dataloader(
+                fmt='test',
+                data_path=data_path,
+                batch_size=params['test']['batch_size'],
+                **kwargs
+            )
+
+            gen_model = MNISTModel().to(device)
+            gen_optimizer = optim.Adadelta(gen_model.parameters(), lr=params['train']['lr'])
+
+            checkpointer = Checkpointer(
+                model=gen_model,
+                optimizer=gen_optimizer,
+                model_path=os.path.join(MODELS_ROOT, args.generator)
+            )
+            checkpointer.restore(use_checkpoint=True)
+
+        gen_model.eval() # NOTE only used for feature generation
+        model = GPCNNModel(
+            feature_extractor=gen_model,
+            num_dim=params['num_features'],
+            num_classes=params['num_classes'],
+            num_data=len(train_loader)
+        ).to(device)
+
+        optimizer = optim.SGD([
+            {'params': model.feature_extractor.parameters(), 'weight_decay': 1e-4},
+            {'params': model.gp_layer.hyperparameters(), 'lr': params['train']['lr'] * 0.01},
+            {'params': model.gp_layer.variational_parameters()},
+            {'params': model.likelihood.parameters()},
+        ], lr=params['train']['lr'], momentum=0.9, nesterov=True, weight_decay=0)
+
+        trainer = GPCNNTrainer(
+            model=model,
+            optimizer=optimizer,
+            save_every_n=params['train']['save_every_n'],
+            model_path=model_path,
+            use_checkpoint=args.use_checkpoint
+        )
+        trainer(train_loader, valid_loader, test_loader, params['train']['epochs'], device)
+
     else:
         raise Exception(f'Unsupported model of type {args.model}')
 
