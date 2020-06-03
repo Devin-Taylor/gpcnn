cbb03dccf3fbb5d4ae43a80c4942f4888b4a5c21
Devin Taylor <dev.t03@gmail.com>
2020-06-03 23:47:04 +0200

    added Gaussian Process layer

---
+++ gpcnn/models/gp/model.py
@@ -0,0 +1,37 @@
+"""
+Adapted from https://gpytorch.readthedocs.io/en/latest/examples/06_PyTorch_NN_Integration_DKL/Deep_Kernel_Learning_DenseNet_CIFAR_Tutorial.html
+"""
+import math
+
+import gpytorch
+import torch
+
+
+class GPLayer(gpytorch.models.ApproximateGP):
+    def __init__(self, num_dim, grid_bounds=(-10., 10.), grid_size=64):
+        variational_distribution = gpytorch.variational.CholeskyVariationalDistribution(
+            num_inducing_points=grid_size, batch_shape=torch.Size([num_dim])
+        )
+
+        variational_strategy = gpytorch.variational.MultitaskVariationalStrategy(
+            gpytorch.variational.GridInterpolationVariationalStrategy(
+                self, grid_size=grid_size, grid_bounds=[grid_bounds],
+                variational_distribution=variational_distribution,
+            ), num_tasks=num_dim,
+        )
+        super().__init__(variational_strategy)
+
+        self.covar_module = gpytorch.kernels.ScaleKernel(
+            gpytorch.kernels.RBFKernel(
+                lengthscale_prior=gpytorch.priors.SmoothedBoxPrior(
+                    math.exp(-1), math.exp(1), sigma=0.1, transform=torch.exp
+                )
+            )
+        )
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.grid_bounds = grid_bounds
+
+    def forward(self, x):
+        mean = self.mean_module(x)
+        covar = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean, covar)
